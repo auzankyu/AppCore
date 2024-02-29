33f4596df8cb80ade95784d64394df69407eda45
Auzan Lazuardi <auzanlazuardi@gmail.com>
2024-02-29 21:29:30 +0700

    Initial Commit

---
+++ AppCore/AppCore.h
@@ -0,0 +1,18 @@
+//
+//  AppCore.h
+//  AppCore
+//
+//  Created by Auzan Lazuardi on 29/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppCore.
+FOUNDATION_EXPORT double AppCoreVersionNumber;
+
+//! Project version string for AppCore.
+FOUNDATION_EXPORT const unsigned char AppCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppCore/PublicHeader.h>
+
+
