49c4419ac1bf692bacb41a4c7482be4cccc85277
Auzan Lazuardi <auzanlazuardi@gmail.com>
2024-02-29 21:45:45 +0700

    initial commit

---
+++ AppCore/AppCore.swift
@@ -0,0 +1,16 @@
+//
+//  AppCore.swift
+//  AppCore
+//
+//  Created by Auzan Lazuardi on 29/02/24.
+//
+
+import Foundation
+
+public class AppCore {
+    public init() {}
+    
+    public func start() {
+        print("AppCore Framework")
+    }
+}
